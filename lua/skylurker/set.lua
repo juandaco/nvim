ca0e423a32941b46eafe36cc09157b9e9fde7b10
Juan D. Acosta <juandacorias@gmail.com>
2024-12-27 07:21:03 -0500

    Updated versions, blinking cursor.

--- lua/skylurker/set.lua
+++ lua/skylurker/set.lua
@@ -1,11 +1,12 @@
 vim.opt.termguicolors = true
+vim.opt.guicursor = 'n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250'
 
 vim.opt.number = true
 vim.opt.relativenumber = true
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 
